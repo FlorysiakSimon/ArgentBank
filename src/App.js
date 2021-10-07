e9c3cd39d8704d14eb9f3dfdbe08fca17922ac70
simon <simon.florysiak@gmail.com>
2021-10-07 19:33:22 +0200

    html/scss update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
+import LoginPage from './pages/LoginPage/LoginPage'
 import Footer from "./components/Footer/Footer";
 import Nav from "./components/Nav/Nav";
+import UserPage from './pages/UserPage/UserPage';
 
 export default function App() {
   return (
     <Router>
       <Nav/>            
       <Switch>
-        <Route exact path="/" component={HomePage} />  
+        <Route exact path="/" component={HomePage} />
+        <Route path='/login' component={LoginPage} />
+        <Route path='/user' component={UserPage} />
       </Switch>
       <Footer/>
     </Router>
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,23 +1,20 @@
 import React from 'react'
 import './Nav.scss';
 import logo from '../../assets/img/argentBankLogo.png';
+import {Link} from 'react-router-dom'
 
 export default function Nav() {
     return (
             <nav className="main-nav">
-                <a className="main-nav-logo" href="./index.html">
-                    <img
-                    className="main-nav-logo-image"
-                    src={logo}
-                    alt="Argent Bank Logo"
-                    />
+                <Link className="main-nav-logo" to="/">
+                    <img className="main-nav-logo-image" src={logo} alt="Argent Bank Logo"/>
                     <h1 className="sr-only">Argent Bank</h1>
-                </a>
+                </Link>
                 <div>
-                    <a className="main-nav-item" href="./sign-in.html">
-                    <i className="fa fa-user-circle"></i>
-                    Sign In
-                    </a>
+                    <Link className='main-nav-item' to='/login'>
+						<i className='fa fa-user-circle'></i>
+						Sign In
+					</Link>
                 </div>
             </nav>
     )
